797959c8dba104841655ad29393b3ad1e2640b0d
chengtongda@163.com <chengtongda@163.com>
2017-11-02 16:51:12 +0800

    merge

--- bin/install.sh
+++ bin/install.sh
@@ -1,17 +1,44 @@
-#! /bin/ksh
+#! /bin/bash
 
-typeset GREYS_FILE="greys.zip";
+# temp file of greys.sh
+TEMP_GREYS_FILE="./greys.sh.$$"
 
-echo "Download... greys.zip";
-curl -sLk "http://ompc.oss-cn-hangzhou.aliyuncs.com/greys/greys.zip" -o $GREYS_FILE;
-if [[ ! $? -eq  0 ]]; then
-    echo "download file failed!";
-    exit -1;
-fi 
+# target file of greys.sh
+TARGET_GREYS_FILE="./greys.sh"
 
-unzip $GREYS_FILE
-rm -rf $GREYS_FILE
-chmod +x greys/greys
+# update timeout(sec)
+SO_TIMEOUT=60
 
-echo "done. enjoy yourself.^_^"
 
+# exit shell with err_code
+# $1 : err_code
+# $2 : err_msg
+exit_on_err()
+{
+    [[ ! -z "${2}" ]] && echo "${2}" 1>&2
+    exit ${1}
+}
+
+# check permission to download && install
+[ ! -w ./ ] && exit_on_err 1 "permission denied, target directory ./ was not writable."
+
+# download from aliyunos
+echo "downloading... ${TEMP_GREYS_FILE}";
+curl \
+    -sLk \
+    --connect-timeout ${SO_TIMEOUT} \
+    "http://ompc.oss.aliyuncs.com/greys/greys.sh" \
+    -o ${TEMP_GREYS_FILE} \
+|| exit_on_err 1 "download failed!"
+
+# check download file format
+[[ -z $(grep "desc : write for july" ${TEMP_GREYS_FILE}) ]] \
+&& exit_on_err 1 "download failed!"
+
+# wirte or overwrite local file
+rm -rf greys.sh
+mv ${TEMP_GREYS_FILE} ${TARGET_GREYS_FILE}
+chmod +x ${TARGET_GREYS_FILE}
+
+# done
+echo "greys install successed."
